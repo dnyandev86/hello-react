b69818ae4481eee49106cf62d372ab85eedeafeb
Dnyandev Warik <dnyandev86@gmail.com>
2017-01-29 21:35:07 -0600

    more props

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,26 @@
 var Greeter = React.createClass({
+  getDefaultProps: function(){
+    return {
+      name: 'React',
+      message: 'This is from compoment default props'
+    };
+  },
   render: function () {
     var name = this.props.name;
+    var message = this.props.message;
 
     return (
       <div>
         <h1>Hello {name} !!!</h1>
-        <p>This is from react component</p>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
+const firstName= "Dnyandev";
+
 ReactDOM.render(
-  <Greeter name="Dnyandev"/>,
+  <Greeter name={firstName} message="This is from props"/>,
   document.getElementById('app')
 );
